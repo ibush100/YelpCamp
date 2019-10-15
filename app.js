648821edf1142700e3d0eacaec138624f828ea96
Ian Bush <ianfracnisbush@gmail.com>
2019-10-15 09:10:34 -0400

    created landing page

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-var express = require(express);
+var express = require("express");
 var app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+res.render("landing");
+});
+
+app.get("/campgrounds", (req, res) => {
+var campgrounds = [ 
+    {name: "New Symrna Beach", image: "https://ccsearch.creativecommons.org/photos/32b70627-867f-4980-b85f-45b75919b779"},
+    {name: "PlayaLinda", image: "https://farm6.staticflickr.com/5542/12121979263_6bfe23485e_b.jpg"}
+]
+res.render("campgrounds", {campgrounds:campgrounds});
+});
 
 app.listen(3000, () => {
 	console.log('server listening on port 3000');
