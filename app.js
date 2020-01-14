d253f3d5cf098cf545ea33b196dd5bda54ed1c01
Ian Bush <ianfracnisbush@gmail.com>
2020-01-14 13:38:52 -0500

    moving things around in the views

--- app.js
+++ app.js
@@ -14,18 +14,19 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
-
+//INDEX
 app.get("/campgrounds", function(req, res){
     // Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds){
        if(err){
            console.log(err);
        } else {
-          res.render("index",{campgrounds:allCampgrounds});
+          res.render("campgrounds/index",{campgrounds:allCampgrounds});
        }
     });
 });
 
+//CREATE
 app.post("/campgrounds", function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
@@ -43,11 +44,12 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+//NEW 
 app.get("/campgrounds/new", function(req, res){
-   res.render("new.ejs"); 
+   res.render("campgrounds/new.ejs"); 
 });
 
-// SHOW - shows more info about one campground
+// SHOW 
 app.get("/campgrounds/:id", function(req, res){
     //find the campground with provided ID
     Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
@@ -56,11 +58,18 @@ app.get("/campgrounds/:id", function(req, res){
         } else {
             console.log(foundCampground)
             //render show template with that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
 });
 
+//===============
+//Coment Routes
+//===============
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+res.render("comments/new")
+});
 
 app.listen(3000, () => {
 	console.log('server listening on port 3000');
